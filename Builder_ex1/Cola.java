45bcab4227da34d642fe208b710805e5df1a2544
Crihan Diana <32567337+CrihanDiana@users.noreply.github.com>
2021-11-04 17:35:05 +0200

    Add files via upload

---
+++ Builder_ex1/Cola.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Cola extends ColdDrink{
+    @Override
+    public float price() {
+        return 30.0f;
+    }
+
+    @Override
+    public String name() {
+        return "Coke";
+    }
+}
